ea3b1737cd1fe3bfad6e5538805370c82bd581a7
zengqingf <work@WorkdeMacBook-Pro.local>
2015-07-29 15:37:53 +0800

    buyao

---
+++ ImageRotate/ImageRotate WatchKit Extension/InterfaceController.h
@@ -0,0 +1,14 @@
+//
+//  InterfaceController.h
+//  ImageRotate WatchKit Extension
+//
+//  Created by Work on 15/7/29.
+//  Copyright (c) 2015年 zengqingfu. All rights reserved.
+//
+
+#import <WatchKit/WatchKit.h>
+#import <Foundation/Foundation.h>
+
+@interface InterfaceController : WKInterfaceController
+
+@end
---
+++ ImageRotate/ImageRotate WatchKit Extension/InterfaceController.m
@@ -0,0 +1,38 @@
+//
+//  InterfaceController.m
+//  ImageRotate WatchKit Extension
+//
+//  Created by Work on 15/7/29.
+//  Copyright (c) 2015年 zengqingfu. All rights reserved.
+//
+
+#import "InterfaceController.h"
+
+
+@interface InterfaceController()
+
+@end
+
+
+@implementation InterfaceController
+
+- (void)awakeWithContext:(id)context {
+    [super awakeWithContext:context];
+
+    // Configure interface objects here.
+}
+
+- (void)willActivate {
+    // This method is called when watch view controller is about to be visible to user
+    [super willActivate];
+}
+
+- (void)didDeactivate {
+    // This method is called when watch view controller is no longer visible
+    [super didDeactivate];
+}
+
+@end
+
+
+
